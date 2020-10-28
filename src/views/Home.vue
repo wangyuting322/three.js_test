0e7c36ad47c82d7fe40d8519e2162e7df196c6c8
wyt <2428282291@qq.com>
2020-10-28 16:10:37 +0800

    feat -> 添加纹理切换

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
 import * as THREE from 'three'
 // import OrbitControls from 'three/examples/js/controls/OrbitControls'
 import 'three-orbitcontrols';
+import { log } from 'three';
 
 export default {
   name: 'Home',
@@ -12,6 +13,8 @@ export default {
       renderer: null,
       mesh1: null,
       mesh2: null,
+      mesh3: null,
+      mesh4: null,
     }
   },
   methods: {
@@ -99,6 +102,46 @@ export default {
       // mesh.rotateOnAxis(axis,Math.PI/8);//绕axis轴旋转π/8//模型对象的旋转
       this.scene.add(this.mesh2);//网格模型添加到场景中
     },
+    createMesh3() {
+      // 纹理贴图映射到一个矩形平面上
+      let geometry = new THREE.BoxGeometry(100, 100, 100); //创建一个球体几何对象，入参为半径和精度
+      // TextureLoader创建一个纹理加载器对象，可以加载图片作为几何体纹理
+      var textureLoader = new THREE.TextureLoader();
+      // 执行load方法，加载纹理贴图成功后，返回一个纹理对象Texture
+      textureLoader.load(require('../assets/logo.png'), (texture) => {
+        var material = new THREE.MeshLambertMaterial({
+          //   color: 0x0000ff,
+          // 设置颜色纹理贴图：Texture对象作为材质map属性的属性值
+          map: texture,//设置颜色贴图属性值
+        }); //材质对象Material
+        this.mesh3 = new THREE.Mesh(geometry, material); //网格模型对象Mesh
+        this.scene.add(this.mesh3);//网格模型添加到场景中
+      })
+    },
+    createMesh4() {
+      // 纹理贴图映射到一个矩形平面上
+      let geometry = new THREE.BoxGeometry(100, 100, 100); //创建一个球体几何对象，入参为半径和精度
+
+      var canvas = document.querySelector(".canvas");
+      var c = canvas.getContext('2d');
+      var myImage = new Image();
+      myImage.src = require('../assets/back4.jpg');
+
+      myImage.onload = () => {
+        c.drawImage(myImage, 0, 0, 100, 100)
+        var texture = new THREE.CanvasTexture(canvas);
+        var material = new THREE.MeshPhongMaterial({
+        //   color: 0xffffff,
+        //   specular: 0x4488ee,
+        //   shininess: 10,
+          // 设置颜色纹理贴图：Texture对象作为材质map属性的属性值
+          map: texture,//设置颜色贴图属性值
+        }); //材质对象Material
+        this.mesh4 = new THREE.Mesh(geometry, material); //网格模型对象Mesh
+        this.scene.add(this.mesh4);//网格模型添加到场景中
+      }
+
+    },
     /**
      * 初始化，创建场景（网格模型、光源）、相机、控制器、渲染器
      * @doc http://www.webgl3d.cn/Three.js/
@@ -109,11 +152,15 @@ export default {
        * 创建场景
        */
       this.scene = new THREE.Scene();
+      this.scene.background = new THREE.Color(0xf1f1f1);
+      //   this.scene.fog = new THREE.Fog(0xf1f1f1, 20, 100);
       /**
        * 创建一个网格模型
        */
-      this.createMesh1();
-      this.createMesh2();
+      //   this.createMesh1();
+      //   this.createMesh2();
+      //   this.createMesh3();
+      this.createMesh4();
       /**
      * 辅助坐标轴
      */
@@ -139,8 +186,8 @@ export default {
       this.camera.position.set(200, 300, 200); //设置相机位置
       this.camera.lookAt(this.scene.position); //设置相机方向(指向的场景对象)
 
-      //   this.camera = new THREE.PerspectiveCamera(70, container.clientWidth / container.clientHeight, 0.01, 10);//创建一个透视相机
-      //   this.camera.position.z = 1;
+      // this.camera = new THREE.PerspectiveCamera(70, container.clientWidth / container.clientHeight, 0.01, 10);//创建一个透视相机
+      // this.camera.position.z = 1;
       /**
        * 创建渲染器
        */
@@ -163,8 +210,8 @@ export default {
       this.renderer.render(this.scene, this.camera);//渲染场景和相机
       //   this.mesh.rotation.x += 0.01;
       //   this.mesh.rotation.y += 0.02;
-      this.mesh1.rotateY(0.01)
-      this.mesh2.rotateX(0.01)
+      //   this.mesh1.rotateY(0.01)
+      //   this.mesh2.rotateX(0.01)
       requestAnimationFrame(this.animate);// 告诉浏览器——你希望执行一个动画，并且要求浏览器在下次重绘之前调用指定的回调函数更新动画。该方法需要传入一个回调函数作为参数，该回调函数会在浏览器下一次重绘之前执行
     },
   },
@@ -174,6 +221,7 @@ export default {
   render() {
     return (
       <div>
+        <canvas class='canvas'  width="100" height="100"></canvas>
         <div id='container'></div>
       </div>
     )
